8b649a4a15f7610882f227e642342fe37ca99408
massouty <massouty@outlook.com>
2022-08-17 14:37:11 +0200

    Create index.js
    If applied , this commit will have added and edited file(index.js).

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/html')
+  res.write('hello:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
